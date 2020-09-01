3cf82fad517eecdc933bbfa35e8a453b7b7c3994
shanxi85 <shanxi_gill85@yahoo.com>
2020-09-01 23:13:26 +0800

    fit size image

--- server.js
+++ server.js
@@ -15,8 +15,42 @@ app.get('/about', (req, res)=>{
   res.render('about');
 });
 
-app.get('/project', (req, res)=>{
-  res.render('project');
+const projectlist = [
+
+{
+logo: 'https://i.ytimg.com/vi/-dSeD-hLNZQ/hqdefault.jpg',
+company: 'Mini Project',
+description: 'Chat Application',
+tech: 'Android Application',
+category: 'Android'
+},
+{
+logo: 'https://www.ebuyer.com/blog/wp-content/uploads/2014/07/buttons-on-a-calculator-header1.jpg',
+company: 'Mini Project',
+description: 'Calculator',
+tech: 'Website',
+category: 'Fullstack'
+},
+{
+logo: 'https://i.pinimg.com/originals/a7/1a/fc/a71afc4425785506e03078b742c6b64e.png',
+company: 'Mini Project',
+description: 'BMI-Calculator',
+tech: 'Android Application',
+category: 'Android'
+},
+{
+logo: 'https://i.ytimg.com/vi/-dSeD-hLNZQ/hqdefault.jpg',
+company: 'Mini Project',
+description: 'Chat Application',
+tech: 'Android Application',
+category: 'Android'
+}
+
+]
+
+app.get('/projek', (req, res)=>{
+  res.render('projek', { projek: projectlist })
+
 });
 
 app.get('/about', (req, res)=>{
